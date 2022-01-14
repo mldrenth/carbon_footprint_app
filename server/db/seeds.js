c3995d5c68cc08fd0954b290128e5fd1395dea29
Adam Kidd <adamkidd3@gmail.com>
2022-01-14 13:43:08 +0000

    random values for testing

--- server/db/seeds.js
+++ server/db/seeds.js
@@ -1,4 +1,20 @@
 use climateProject_db
 db.dropDatabase()
 
-db.climateData.insertMany()
+db.climateData.insertMany(
+    [
+        {
+            driving: {
+                electric: 0.1,
+                diesel: 5
+            }
+        },
+        {
+            diet: {
+                vegan: 1000,
+                vegetarian: 2000,
+                meat_eater: 3000
+            }
+        }
+    ]
+)
