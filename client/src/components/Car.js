b9c321c0d76f8f39ca3ea81d76fea0b0b2a36374
Adam Kidd <adamkidd3@gmail.com>
2022-01-14 19:24:54 +0000

    Car dropdown menus and mileage input

--- client/src/components/Car.js
+++ client/src/components/Car.js
@@ -1,30 +1,52 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 const Car = ({ climateData, handleCarCalculation }) => {
 
-    const [co2PerMile, setCo2PerMile] = useState(0);
+    const [co2PerMile, setCo2PerMile] = useState(0.295);
     const [yearlyMileage, setYearlyMileage] = useState(7800);
+    const [fuelType, setFuelType] = useState('petrol');
+    const [carSize, setCarSize] = useState('medium')
 
-    const onChange = (evt) => {
-        setYearlyMileage(evt.target.value)
+    const onMileageChange = (evt) => {
+        setYearlyMileage(evt.target.value);
+        // handleCarCalculation(co2PerMile, yearlyMileage);
     }
 
-    const onSelected = (evt) => {
-        const carType = evt.target.value
-        setCo2PerMile(climateData[0].drivingKgCO2ePerMile[carType])
+    const onFuelSelected = (evt) => {
+        const selectedFuel = evt.target.value;
+        setFuelType(evt.target.value);
+        setCo2PerMile(climateData[0].drivingKgCO2ePerMile[selectedFuel][carSize]);
+        // handleCarCalculation(co2PerMile, yearlyMileage);
+    }
+
+    const onSizeSelected = (evt) => {
+        const selectedSize = evt.target.value;
+        setCarSize(evt.target.value);
+        setCo2PerMile(climateData[0].drivingKgCO2ePerMile[fuelType][selectedSize]);
+        // handleCarCalculation(co2PerMile, yearlyMileage);
     }
 
     return(
         <div>
             <h3>How many miles do you drive per year?</h3>
             <p>The average UK family drives 7800 miles per year</p>
-            <select onChange={onSelected}>
-                <option value={0}></option>
-                <option value={'averageUKFamilyCar'}>average car</option>
+            <label>Select your fuel type </label>
+            <select onChange={onFuelSelected}>
+                <option value="petrol">Petrol (standard)</option>
+                <option value="diesel">Diesel</option>
             </select>
-            <label htmlFor="milesPerYear">X (from state) miles per year:</label>
             <br/>
-            <input type="number" value={yearlyMileage} onChange={onChange}/>
+            <label>Select your car size </label>
+            <select onChange={onSizeSelected}>
+                <option value="medium">Average UK Family Car</option>
+                <option value="small">Small</option>
+                <option value="medium">Medium</option>
+                <option value="large">Large</option>
+            </select>
+            <br/>
+            <label htmlFor="milesPerYear">Enter your yearly mileage </label>
+            <input type="number" value={yearlyMileage} onChange={onMileageChange}/>
+            <p>{co2PerMile}</p>
         </div>
     );
 };
