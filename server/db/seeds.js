5c1501e321c04a03db32c251dd36c2d4a515dfcd
Adam Kidd <adamkidd3@gmail.com>
2022-01-14 14:31:30 +0000

    adds seed data for calculations

--- server/db/seeds.js
+++ server/db/seeds.js
@@ -1,20 +1,53 @@
-use climateProject_db
-db.dropDatabase()
+use climateProject_db;
+db.dropDatabase();
 
 db.climateData.insertMany(
     [
         {
-            driving: {
-                electric: 0.1,
-                diesel: 5
+            drivingKgCO2ePerMile: {
+                averageUKFamilyCar: 0.295,
+                diesel: {
+                    small: 0.22,
+                    medium: 0.25,
+                    large: 0.28
+                },
+                petrol: {
+                    small: 0.27,
+                    medium: 0.31,
+                    large: 0.35
+                },
+                electric: {
+                    lowEfficiency: 0.12,
+                    mediumEfficiency: 0.10,
+                    highEfficiency: 0.08
+                }
             }
         },
         {
-            diet: {
-                vegan: 1000,
-                vegetarian: 2000,
-                meat_eater: 3000
+            dietTonnesCO2ePerYear: {
+                vegan: 1056,
+                vegetarian: 1392,
+                lowMeat: 1706,
+                mediumMeat: 2056,
+                highMeat: 2626
+            }
+        },
+        {
+            electricityKgCO2ePerKWh: {
+                average: 0.309
+            }
+        },
+        {
+            gasKgCO2ePerKWh: {
+                average: 0.203
+            }
+        },
+        {
+            flightKgCO2eOneWay: {
+                domestic: 90,
+                shortHaul: 230,
+                longHaul: 480,
             }
         }
     ]
-)
+);
