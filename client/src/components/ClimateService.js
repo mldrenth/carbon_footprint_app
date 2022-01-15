5760977a3d05d746b8db4af7628c5e8cee608540
Tony <NodeToNowhere@gmail.com>
2022-01-15 15:27:52 +0000

    Client Diet

--- client/src/components/ClimateService.js
+++ client/src/components/ClimateService.js
@@ -1,11 +1,7 @@
 const baseURL = "http://localhost:5000/footprint/"; //Better name? 
 
 export const getData = () => {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve(fetch(baseURL).then((res) => res.json())) //Testing Promise-resolve to see if fixes async data load issue in components.
-    },1500)
-  })
+  return fetch(baseURL).then((res) => res.json());
 };
 
 export const postData = (booking) => {
