5024ad7e211ff51d8a94638aff8e238a59c6997c
Tony <NodeToNowhere@gmail.com>
2022-01-15 16:03:06 +0000

    Diet Form branch fix

--- client/src/containers/FormContainer.js
+++ client/src/containers/FormContainer.js
@@ -1,7 +1,8 @@
 import Car from '../components/Car';
 import { useEffect, useState } from 'react';
+import DietForm from '../components/Dietform'
 
-const FormContainer = ({ handleCarCalculation, climateData }) => {
+const FormContainer = ({ handleCarCalculation, climateData, handleDietCalculation }) => {
 
     // const [carData, setCarData] = useState({});
     //create states for other data types here
@@ -18,6 +19,7 @@ const FormContainer = ({ handleCarCalculation, climateData }) => {
             handleCarCalculation={handleCarCalculation}
             climateData={climateData}
             />
+            <DietForm climateData={climateData} handleDietCalculation={handleDietCalculation} />
         </div>
     );
 };
