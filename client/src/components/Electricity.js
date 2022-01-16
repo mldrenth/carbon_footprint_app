0a4a9904f10ea0fcec0f063b9d89611a63b0562a
Marijke Drenth <drenth.marijke@googlemail.com>
2022-01-16 22:27:07 +0200

    Ads electricity tests and fixes

--- client/src/components/Electricity.js
+++ client/src/components/Electricity.js
@@ -1,24 +1,24 @@
 import React, { useState, useEffect } from "react";
 
 const Electricity = ({ handleElectricityCalculation, climateData }) => {
-  const [energyUsage, setEnergyUsage] = useState(4500);
+    const [energyUsage, setEnergyUsage] = useState(4500);
 
-  useEffect(() => {
-    handleElectricityCalculation(energyUsage);
-  }, [energyUsage]);
+    useEffect(() => {
+        handleElectricityCalculation(energyUsage);
+    }, [energyUsage]);
 
-  const onChange = (eve) => {
-    setEnergyUsage(eve.target.value);
-  };
+    const onChange = (eve) => {
+      return (eve.target.value > 0 ? setEnergyUsage(eve.target.value) : null);
+    };
 
-  return (
-    <>
-      <h3>Electricity</h3>
-      <p> How much energy do you use per year?</p>
-      <input id="electricity-input" onChange={onChange} type="number" min={0} step={100} placeholder="4500"></input>
-      <span>kWh</span>
-    </>
-  );
+    return (
+        <>
+            <h3>Electricity</h3>
+            <p> How much energy do you use per year?</p>
+            <input id="electricity-input" onChange={onChange} type="number" min={0} step={100} placeholder="4500" ></input>
+            <span> kWh </span>
+        </>
+    );
 };
 
 export default Electricity;
